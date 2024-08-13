a11d6dde4a2b7468dbabe21265c5a31c92249337
Ahmed Elzaki <ahmedelzakiomer@gmail.com>
2024-08-13 11:39:12 +0300

    Update submit.js

--- js/submit.js
+++ js/submit.js
@@ -1,14 +1,32 @@
-document.getElementById("contactForm").addEventListener("submit", function(event){
-    event.preventDefault();
+// Initialize EmailJS
+(function(){
+   emailjs.init("wdu6udTZ4OIVrOcRE");
+})();
 
-    var formData = new FormData(this);
+function sendEmail() {
+   var name = document.getElementById("name").value;
+   var email = document.getElementById("email").value;
+   var phone = document.getElementById("phone").value;
+   var message = document.getElementById("message").value;
+   
+   console.log(name);  // Log name inside the function after user input just for debugging
 
-    fetch("sendEmail.php", {
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => alert(data))
-    .catch(error => console.error('Error:', error));
-});
-
+   emailjs.send("service_k5fnrsv","template_uqq5uck",{
+      from_name: name,
+      from_email: email,
+      phone: phone,
+      message: message,
+   })
+   .then(function(response) {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Message sent successfully!");
+      // Clear the input fields
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("message").value = "";
+   }, function(error) {
+      console.log("FAILED...", error);
+      alert("Failed to send the message.");
+   });
+}
